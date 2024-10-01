7d12d00e6d36c45d47ab70fcd27c6ed9481a87bc
ANGEL-DGP <angel.patinogd@gmail.com>
2024-10-01 13:59:53 -0500

    formulario

---
+++ src/components/MyDocument.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 20,
+  },
+  table: {
+    display: "table",
+    width: "auto",
+    margin: "auto",
+  },
+  tableRow: {
+    flexDirection: "row",
+  },
+  tableCell: {
+    border: "1pt solid black",
+    padding: 5,
+    width: "50%",
+  },
+  subtitle: {
+    fontWeight: "bold",
+  },
+});
+
+const MyDocument = ({ dataList }) => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View>
+        <Text>Datos del formulario:</Text>
+        <View style={styles.table}>
+          {dataList.map((data, index) => (
+            <React.Fragment key={index}>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>Sede:</Text>
+                <Text style={styles.tableCell}>{data.sede}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Código Carrera:
+                </Text>
+                <Text style={styles.tableCell}>{data.c_carrera}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Código Convenio:
+                </Text>
+                <Text style={styles.tableCell}>{data.c_convenio}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Nombre Convenio:
+                </Text>
+                <Text style={styles.tableCell}>{data.n_convenio}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Código Internacional:
+                </Text>
+                <Text style={styles.tableCell}>{data.c_internacional}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>País:</Text>
+                <Text style={styles.tableCell}>{data.pais}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Actividades:
+                </Text>
+                <Text style={styles.tableCell}>{data.actividades}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Fecha de Inicio:
+                </Text>
+                <Text style={styles.tableCell}>{data.f_inicio}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Fecha de Fin:
+                </Text>
+                <Text style={styles.tableCell}>{data.f_fin}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  A Conocimiento:
+                </Text>
+                <Text style={styles.tableCell}>{data.a_conocimiento}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={[styles.tableCell, styles.subtitle]}>
+                  Financiamiento:
+                </Text>
+                <Text style={styles.tableCell}>{data.financiamiento}</Text>
+              </View>
+            </React.Fragment>
+          ))}
+        </View>
+      </View>
+    </Page>
+  </Document>
+);
+
+export default MyDocument;
---
+++ src/components/Pa.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  CButton,
+  CCol,
+  CContainer,
+  CModal,
+  CModalBody,
+  CModalHeader,
+  CModalTitle,
+  CRow,
+} from "@coreui/react";
+import FormLogins from "./FormLogins";
+import MyDocument from "./MyDocument";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+
+export default function Pa() {
+  const [visible, setVisible] = useState(false);
+  const [dataList, setDataList] = useState([]); // Estado para almacenar los datos del formulario
+
+  // Función para actualizar los datos
+  const handleDataSubmit = (data) => {
+    setDataList([...dataList, data]);
+  };
+
+  return (
+    <>
+      <CContainer>
+        <CRow className="mt-5">
+          <h1>INTERNACIONALIZACIÓN</h1>
+        </CRow>
+        <CRow className="mt-2 mb-2">
+          <CCol className="mt-2 mb-2">
+            <h3>Usuario Actual</h3>
+          </CCol>
+          <CCol>
+            <input disabled value={"Formativa.ti@pucese.edu.ec"}></input>
+          </CCol>
+          <CCol>
+            <CButton color="primary" onClick={() => setVisible(!visible)}>
+              +
+            </CButton>
+          </CCol>
+        </CRow>
+      </CContainer>
+      <CModal
+        scrollable
+        size="xl"
+        visible={visible}
+        onClose={() => setVisible(false)}
+        aria-labelledby="ScrollingLongContentExampleLabel2"
+      >
+        <CModalHeader>
+          <CModalTitle id="ScrollingLongContentExampleLabel2">
+            Documento
+          </CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <FormLogins onDataSubmit={handleDataSubmit} />
+          <PDFDownloadLink
+            document={<MyDocument dataList={dataList} />}
+            fileName="Mi_pdf.pdf"
+          >
+            {({ loading }) =>
+              loading ? (
+                <button>Cargando...</button>
+              ) : (
+                <button>Descargar</button>
+              )
+            }
+          </PDFDownloadLink>
+        </CModalBody>
+      </CModal>
+    </>
+  );
+}
